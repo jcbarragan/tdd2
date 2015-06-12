7cf0dd4292d50969841442b474fa087144aef2df
Juan <jcbarragan@live.com>
2015-06-11 17:31:30 -0700

    hardest js challenge in the world

---
+++ index.js
@@ -0,0 +1,12 @@
+/// this is Juan's
+
+var doubler = function(a, b) {
+  return a + a + b + b;
+};
+
+console.assert(doubler(4,2) === 12);
+console.assert(doubler(1,3) === 8);
+console.assert(doubler(-1,3) === 4);
+
+console.assert(doubler("a", "b") === "aabb");
+console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
