04791ace501ce2af88dc5d8dd7b82bf3a95f689d
Juan <jcbarragan@live.com>
2015-06-11 17:38:54 -0700

    be scared!

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 /// this is Juan's
 
 var doubler = function(a, b) {
-  return a + a + b + b;
+  
 };
 
-console.assert(doubler(4,2) === 12);
-console.assert(doubler(1,3) === 8);
-console.assert(doubler(-1,3) === 4);
-
-console.assert(doubler("a", "b") === "aabb");
-console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
+console.assert(doubler(4,99) === 103);
